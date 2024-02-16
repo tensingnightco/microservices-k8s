5a194f8ff726b751f766c7245eda729c123b40c0
thanh <congtuvjppr02k3@gmail.com>
2024-02-16 17:58:45 +0700

    Added my project

---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,53 @@
+import express, { Request, Response } from "express"
+import { body } from "express-validator"
+import jwt from "jsonwebtoken"
+
+import { ValidateRequest } from "../middleware/ValidateRequest"
+import { User } from "../models/user"
+import { BadRequestError } from "../errors/BadRequestError"
+
+const router = express.Router()
+
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  ValidateRequest,
+  async (req: Request, res: Response) => {
+
+
+    const { email, password } = req.body
+
+    const existingUser = await User.findOne({ email })
+
+    if (existingUser) {
+      // console.log('Email in use')
+      // return res.send({})
+      throw new BadRequestError('Email in use')
+    }
+
+    const user = User.build({ email, password })
+    await user.save()
+
+    // Generate JWT
+
+    const userJWT = jwt.sign({
+      id: user.id,
+      email: user.email
+    }, process.env.JWT_KEY!)
+
+    // Store it on session object
+    req.session = {
+      jwt: userJWT
+    }
+
+    res.status(201).send(user)
+  }
+);
+
+export { router as signupRouter }
